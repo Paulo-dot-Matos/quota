6cd623288617e6836569a5b16eb928d9e861efe2
Paulo-dot-Matos <pauloromatos@outlook.pt>
2019-10-31 17:49:18 +0000

    JSON on html ... sort off...

---
+++ src/javaScript.js
@@ -0,0 +1,191 @@
+
+var socios = [
+    {
+      "Num_Socio": 1,
+      "Nome": "Joaquim Barata",
+      "Data_Nascimento": "12/02/1978",
+      "Data_Socio": "04/05/1991",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 2,
+      "Nome": "Nuno Tavares",
+      "Data_Nascimento": "20/07/1993",
+      "Data_Socio": "03/07/1992",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 3,
+      "Nome": "Luisa Amaro",
+      "Data_Nascimento": "14/08/1989",
+      "Data_Socio": "04/05/1991",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 4,
+      "Nome": "Paulo Matos",
+      "Data_Nascimento": "12/04/1987",
+      "Data_Socio": "04/05/1991",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 5,
+      "Nome": "Rodrigo Matos",
+      "Data_Nascimento": "30/12/1979",
+      "Data_Socio": "04/05/1991",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 6,
+      "Nome": "Francisco Domingos",
+      "Data_Nascimento": "01/07/1990",
+      "Data_Socio": "08/09/1994",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 7,
+      "Nome": "Joana Lan�a",
+      "Data_Nascimento": "09/09/1999",
+      "Data_Socio": "04/04/2006",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 8,
+      "Nome": "Maria Costa",
+      "Data_Nascimento": "23/11/1984",
+      "Data_Socio": "05/07/2004",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 9,
+      "Nome": "Pedro Miguel",
+      "Data_Nascimento": "09/03/1979",
+      "Data_Socio": "01/12/1991",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 10,
+      "Nome": "Elisa Ferreira",
+      "Data_Nascimento": "28/04/1981",
+      "Data_Socio": "12/02/1996",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 11,
+      "Nome": "Joaquim Barata",
+      "Data_Nascimento": "12/02/1978",
+      "Data_Socio": "04/05/1991",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 12,
+      "Nome": "Nuno Tavares",
+      "Data_Nascimento": "20/07/1993",
+      "Data_Socio": "03/07/1992",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 13,
+      "Nome": "Luisa Amaro",
+      "Data_Nascimento": "14/08/1989",
+      "Data_Socio": "04/05/1991",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 14,
+      "Nome": "Paulo Matos",
+      "Data_Nascimento": "12/04/1987",
+      "Data_Socio": "04/05/1991",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 15,
+      "Nome": "Rodrigo Matos",
+      "Data_Nascimento": "30/12/1979",
+      "Data_Socio": "04/05/1991",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 16,
+      "Nome": "Francisco Domingos",
+      "Data_Nascimento": "01/07/1990",
+      "Data_Socio": "08/09/1994",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 17,
+      "Nome": "Joana Lan�a",
+      "Data_Nascimento": "09/09/1999",
+      "Data_Socio": "04/04/2006",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 18,
+      "Nome": "Maria Costa",
+      "Data_Nascimento": "23/11/1984",
+      "Data_Socio": "05/07/2004",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 19,
+      "Nome": "Pedro Miguel",
+      "Data_Nascimento": "09/03/1979",
+      "Data_Socio": "01/12/1991",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    },
+    {
+      "Num_Socio": 20,
+      "Nome": "Elisa Ferreira",
+      "Data_Nascimento": "28/04/1981",
+      "Data_Socio": "12/02/1996",
+      "Morada": "Morada x",
+      "email": "x@ansjndasjkf.pt",
+      "telefone": 98222444
+    }
+   ];
+
+
+
+var texto = JSON.stringify(socios[0]);
+
+
+
+document.getElementById('demo').innerHTML = texto;
